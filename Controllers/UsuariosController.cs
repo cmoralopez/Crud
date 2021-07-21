9237565bc669df1626669c33ff6a145cba9c88c5
Christian Mora <cmoralopez@gmail.com>
2021-07-21 17:15:00 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,118 @@
+﻿using Crud.Data;
+using Crud.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsuariosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Usuario> usuarios = new List<Usuario>();
+            try
+            {
+                
+                return View(await _context.Usuario.ToListAsync());
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return View(usuarios);
+        }
+        
+        public IActionResult Index()
+        {
+            List<Usuario> usuarios = new List<Usuario>();
+            usuarios = _applicationDbContext.Usuario.ToList();
+            return View(usuarios);
+        }
+
+        // GET: UsuariosController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UsuariosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsuariosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Usuario usuario)
+        {
+            try
+            {
+                List<Usuario> usuarios = new List<Usuario>();
+                usuarios = JsonConvert.DeserializeObject<List<Usuario>>(HttpContext.Session.GetString("ListaPersona"));
+                usuarios.Add(usuario);
+                HttpContext.Session.SetString("ListaPersona", JsonConvert.SerializeObject(usuarios));
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuariosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuariosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuariosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuariosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
+
+
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Crud.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crud.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<Usuario> Usuario { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Usuario>(en =>
+            {
+                en.Haskey(e => e.Codigo);
+
+                en.Property(e => e.Nombre)
+                //.IsRequeried()
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+                en.Property(e => e.Apellido)
+                //.IsRequeried()
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+                en.Property(e => e.Direccion)
+                //.IsRequeried()
+                .HasMaxLength(250)
+                .IsUnicode(false);
+            });
+        }
+    }
+}
